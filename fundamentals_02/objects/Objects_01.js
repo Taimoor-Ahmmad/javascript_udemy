93766e735707df01fa230342b907cc84b2b2543b
Taimoor <72995062+Taimoor-Ahmmad@users.noreply.github.com>
2024-06-25 22:34:41 +0500

    Objects intro

---
+++ fundamentals_02/objects/Objects_01.js
@@ -0,0 +1,22 @@
+//Why we need objects when we had arrays?
+const TaimoorArray = [
+  "Taimoor",
+  "Ahmmad",
+  2000 - 2024,
+  "employee",
+  ["Shane", "Usman", "Kamran"],
+];
+
+//Because in objects we can give elements the name!
+
+//There are multiple ways of creating objects
+//1. Object literal String*/
+
+const Taimoor = {
+  firstName: "Taimoor",
+  lastName: "Ahmmad",
+  age: 23,
+  role: "employee",
+  friends: ["Shane", "Usman", "Kamran"],
+};
+//
