edf53feef95a753c52b4c69c0c2be9f099491a37
Taimoor <72995062+Taimoor-Ahmmad@users.noreply.github.com>
2024-06-26 11:59:05 +0500

    Loops intro

---
+++ fundamentals_02/Loops/Loops_01.js
@@ -0,0 +1,12 @@
+//Analogy of gym--->Liting weights
+
+console.log("Lifting weights 1");
+console.log("Lifting weights 2");
+console.log("Lifting weights 3");
+console.log("Lifting weights 4");
+console.log("Lifting weights 5");
+
+//using for loop=>keeps running until the value is true
+for (let rep = 1; rep <= 5; rep++) {
+  console.log(`Lifting weights ${rep}`);
+}
