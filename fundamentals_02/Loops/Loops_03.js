4ce3e98dcb8fba8974db993138afd7d5dec2d646
Taimoor <72995062+Taimoor-Ahmmad@users.noreply.github.com>
2024-06-26 13:59:14 +0500

    Looping backwards

---
+++ fundamentals_02/Loops/Loops_03.js
@@ -0,0 +1,12 @@
+const Taimoor = [
+  "Taimoor Ahmmad",
+  24,
+  true,
+  "Student",
+  ["Usman", "Ali", "Aslam"],
+];
+
+//looping backwards
+for (let i = Taimoor.length - 1; i >= 0; i--) {
+  console.log(Taimoor[i]);
+}
