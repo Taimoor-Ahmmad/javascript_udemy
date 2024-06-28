51af5ffb234205b3561f075167b6de6b092c021d
Taimoor <72995062+Taimoor-Ahmmad@users.noreply.github.com>
2024-06-28 13:32:42 +0500

    spread operator

--- Data_Structs_&_Modern_Operators/spread_operator.js
+++ Data_Structs_&_Modern_Operators/spread_operator.js
@@ -1,4 +1,11 @@
-//Spread Operator
+//Spread Operator--->basically gets all of the elements out of the array and wrote it manually
+
+//Use case of spread operator
+/*when we want to use all the elements of the array into the function arguments,
+we can use spread operator*/
+const arr1 = [1, 2, 3, 4, 5];
+console.log(arr); //-->1 element which is an array
+console.log(...arr1); //--->all of the elements
 
 //Object for example
 const resturant = {
@@ -48,15 +55,22 @@ console.log(newArr);
 console.log(...newArr);
 console.log(0, 1, 7, 9);
 
-//Creating new Array by expanding old array with new element
+/*Creating new Array by expanding old array with new element
+We are not maipulating resturant.main menu here instead 
+we are creating new Array*/
 const newMenu = [...resturant.mainMenu, "Gnocci"];
 console.log(newMenu);
 
 //Two use cases of spread operator
-//1.Merge two arrays
+
+//1.Copy array
+const mainMenu = [...resturant.mainMenu];
+
+//2.Merge two arrays
 const menu = [...resturant.mainMenu, ...resturant.starterMenu];
 console.log(menu);
 
+//Most of the javascript operators are iterables except objects
 //We can use spread operator on strings as well
 const str = "Jona";
 const letter = [...str, " ", "s ."];
