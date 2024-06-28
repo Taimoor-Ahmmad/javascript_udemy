9ad6272b1bce62eb537c5d7ec70dd521de2c63b8
Taimoor <72995062+Taimoor-Ahmmad@users.noreply.github.com>
2024-06-28 11:30:12 +0500

    spread operator

---
+++ Data_Structs_&_Modern_Operators/spread_operator.js
@@ -0,0 +1,64 @@
+//Spread Operator
+
+//Object for example
+const resturant = {
+  name: "Classico Italino",
+  location: "Los Angales",
+  categories: ["Italian", "Pizzario", "Vegetarian", "Organic"],
+  starterMenu: ["Mocha", "Latte", "Garlic Bread"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (MainIndex, StarterIndex) {
+    return [this.mainMenu[MainIndex], this.StarterMenu[StarterIndex]];
+  },
+  openingHours: {
+    thus: {
+      open: 12,
+      close: 1,
+    },
+    friday: {
+      open: 4,
+      close: 8,
+    },
+    monday: {
+      open: 12,
+      close: 1,
+    },
+    Tuesday: {
+      open: 4,
+      close: 8,
+    },
+  },
+  //destructuring below object
+  orderDelivery: function ({ starterIndex, mainIndex, address, time }) {
+    console.log(
+      `Order Received! ${this.starterMenu[starterIndex]},and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+//for adding elements in an array
+const arr = [1, 7, 9];
+const badarray = [0, arr[0], arr[1], arr[2]];
+console.log(badarray);
+
+//using spread Operator
+const newArr = [0, ...arr];
+console.log(newArr);
+//below both functions are same
+console.log(...newArr);
+console.log(0, 1, 7, 9);
+
+//Creating new Array by expanding old array with new element
+const newMenu = [...resturant.mainMenu, "Gnocci"];
+console.log(newMenu);
+
+//Two use cases of spread operator
+//1.Merge two arrays
+const menu = [...resturant.mainMenu, ...resturant.starterMenu];
+console.log(menu);
+
+//We can use spread operator on strings as well
+const str = "Jona";
+const letter = [...str, " ", "s ."];
+console.log(letter);
+console.log(...str);
