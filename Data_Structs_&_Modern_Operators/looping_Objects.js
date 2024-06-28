8d6966deae0c60b8dd1339aa65878179cc6d5cbd
Taimoor <72995062+Taimoor-Ahmmad@users.noreply.github.com>
2024-06-28 23:07:41 +0500

    looping the objects

---
+++ Data_Structs_&_Modern_Operators/looping_Objects.js
@@ -0,0 +1,12 @@
+const person = {
+  name: "Taimoor",
+  age: 26,
+  email: "taimoor@gmail.com",
+};
+
+const object = Object.keys(person);
+const object1 = Object.values(person);
+const object2 = Object.entries(person);
+console.log(object);
+console.log(object1);
+console.log(object2);
