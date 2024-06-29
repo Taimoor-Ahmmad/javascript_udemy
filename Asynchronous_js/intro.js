aa9c2de7641d24e2d4b76de704fcad9095c4bf74
Taimoor <72995062+Taimoor-Ahmmad@users.noreply.github.com>
2024-06-29 23:17:28 +0500

    Asynchronous js updated

--- Asynchronous_js/intro.js
+++ Asynchronous_js/intro.js
@@ -1,2 +1,42 @@
 /*Most common use case of asynchronous javascript is to fetch data from remote servers
  AJAX calls*/
+
+/*Asynchronous javascript is opposite of synchronous code*/
+
+//Most code is synchronous
+
+//Synchronous code is the one in which code is executed line by line exactly we have defined there!
+//Alert statement is the example of the use case where need of asynchronous javascript need comes into the picture
+//But execution would have to wait for 5 seconds to wait
+
+//Asynchrononus code is the one that is executed after a task that runs in the background finishes
+//It is a non blocking code
+
+//Alert is blocking synchronous code
+
+/*We need a callback function to implement asynchronous code, however that does not mean callback functions make code asynchronous automatically
+
+example:
+    1.Array's map method requires callback function as welllbut that does not mean it makes code asynchoronous.
+    2.Similarly event listeners do not make code asynchronous alone
+*/
+//Asynchronus programming is all about coordinating the behaviour of our program over a certain period of tiem
+
+//Asynchronous means not occuring at the same time.
+
+/*MOST IMPORTANT USE CASE OF JAVASCRIPT
+
+-->1. AJAX calls:
+             Async javascript and XML: Allows us to communicate with remote web servers in an 
+             asynchronous way. With AJAX calls we can request data from server dynamically
+
+*/
+
+/*API
+    Application Programming Interface:
+            Piece of Software that can be used by another piece of software, in order to allow 
+            applications to talk to each other
+
+    DOM API, GEOLOCATION API
+
+*/
