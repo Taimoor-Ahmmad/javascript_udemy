b58d92e30253a3074582ac42203eaf37240cde43
Taimoor <72995062+Taimoor-Ahmmad@users.noreply.github.com>
2024-06-29 22:40:59 +0500

    maps in js

---
+++ Data_Structs_&_Modern_Operators/maps.js
@@ -0,0 +1,9 @@
+/*Maps are used in javascript to map values to keys,
+data is stored in key values pairs in maps, difference
+between maps and objects is that in maps the keys can 
+have any type, but in objects the keys are strings*/
+
+const rest = new Map();
+rest.set("name", "Classico Italino");
+rest.set(1, "Frenzy Italian");
+console.log(rest.set(2, "Taimoor Ahmmad"));
